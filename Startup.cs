52edf93c2406e65861aa77bf3ac08fe7933d6d37
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-18 19:03:30 -0700

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,11 @@ namespace ExploreAdventure
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -23,7 +29,7 @@ namespace ExploreAdventure
         {
             app.UseExceptionHandler("/error.html");
 
-            if (env.IsDevelopment())
+            if (configuration.GetValue<bool>("EnableDeveloperExceptions"))
             {
                 app.UseDeveloperExceptionPage();
             }
