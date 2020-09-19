5d221c21ffe9ff889f54c2c4e4dbd6e0e9afa906
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-19 15:25:33 -0700

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +26,10 @@ namespace ExploreAdventure
             services.AddTransient<FeatureToggles>(x => new FeatureToggles {
                 DeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions")
             });
+
+            services.AddControllersWithViews();
+
+            //MvcOptions.EnableEndpointRouting = false;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +49,13 @@ namespace ExploreAdventure
                 if (context.Request.Path.Value.Contains("invalid"))
                     throw new Exception("ERRor!");
                 await next();
+            });
+
+            app.UseMvc(endpoints =>
+            {
+                endpoints.MapRoute(name:"Default",
+                    "{controller=Home}/{action=Index}/{id?}"
+                   );
             });
 
             app.UseFileServer();
